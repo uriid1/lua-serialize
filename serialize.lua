7304116335247d7c27b4cad2263fb0db5a23c58a
uriid1 <appdurov@gmail.com>
2021-05-26 15:17:12 +0300

    add small fix

--- serialize.lua
+++ serialize.lua
@@ -1,54 +1,56 @@
---[[
-
+--[[  This project is based on https://github.com/rxi/lume
     ####****************************####
     #//# Author: by uriid1          #\\#
     #//# license: NO                #\\#
     #//# telegram: uriid1           #\\#
     #//# Mail: appdurov@gmail.com   #\\#
     ####****************************####
-
-    This project is based on https://github.com/rxi/lume
-
 --]]
 
-local pairs = pairs
-local type  = type
-local tostring, tonumber = tostring, tonumber
-local serialize = {}
+local pairs = pairs;
+local type  = type;
+local tostring = tostring;
+local tonumber = tonumber;
+local serialize = {};
 
--- 
+-- Recursive serialization
 local serialize_map = {
   [ "boolean" ] = tostring,
   [ "string"  ] = function(v) return "'"..tostring(v).."'" end,
-  [ "number"  ] = function(v) return v end,
+  [ "number"  ] = function(v)
+    if      (v ~=  v)     then return  "0/0";      --  nan
+    elseif  (v ==  1 / 0) then return  "1/0";      --  inf
+    elseif  (v == -1 / 0) then return "-1/0"; end  -- -inf
+    return tostring(v)
+  end,
   [ "table"   ] = function(tbl)
-    local tmp = {}
-    for k, v in pairs(tbl) do
-        tmp[#tmp + 1] = "[" .. serialize.create(k) .. "]=" .. serialize.create(v)
+    local tmp = {};
+    for k, v in pairs(tbl) do 
+      tmp[#tmp + 1] = "[" .. serialize.create(k) .. "]=" .. serialize.create(v); 
     end
-    return "{" ..  table.concat(tmp, ",") .. "}"
+    return "{" ..  table.concat(tmp, ",") .. "}";
   end
 }
 
 -- Serialize Table
 function serialize.create(tbl)
-    return serialize_map[type(tbl)](tbl)
+    return serialize_map[type(tbl)](tbl);
 end
 
 -- Load Table
 function serialize.load(fname)
-    local file = io.open(fname)
-    local rfile = file:read("*a")
-    file:close()
-    return assert((loadstring or load)("return"..rfile))()
+    local file = io.open(fname);
+    local rfile = file:read("*a");
+    file:close();
+    return assert((loadstring or load)("return"..rfile))();
 end
 
 -- Save Table
 function serialize.save(tbl, fname)
-    local file = io.open(fname, "w")
-    local wtable = serialize.create(tbl)
-    file:write(wtable)
-    file:close()
+    local file = io.open(fname, "w");
+    local wtable = serialize.create(tbl);
+    file:write(wtable);
+    file:close();
 end
 
-return serialize
+return serialize;
